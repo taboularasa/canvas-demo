595a1403eb1b2480afbf5437ba7bad0a4c9fc81b
David Elliott <david.elliott@stitchfix.com>
2017-05-23 15:46:47 -0700

    can draw a responsive canvas

--- app/index.js
+++ app/index.js
@@ -1,38 +1,59 @@
 let moment = require('moment');
-var stage = new createjs.Stage('main-canvas');
-var shape = new createjs.Shape();
-shape.graphics.beginFill('red').drawRect(0, 0, 120, 120);
-stage.addChild(shape);
-var circle = new createjs.Shape();
-circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-stage.addChild(circle);
+let stage = new createjs.Stage('main-canvas');
 
-let data = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 15, 7: 19, 8: 17, 9: 14, 10: 13, 11: 18, 12: 15, 13: 18, 14: 17, 15: 17, 16: 17, 17: 11, 18: 19, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0}
+(function() {
+    var throttle = function(type, name, obj) {
+        obj = obj || window;
+        var running = false;
+        var func = function() {
+            if (running) { return; }
+            running = true;
+             requestAnimationFrame(function() {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+
+    /* init - you can init any event */
+    throttle("resize", "optimizedResize");
+})();
+
+const rawData = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 15, 7: 19, 8: 17, 9: 14, 10: 13, 11: 18, 12: 15, 13: 18, 14: 17, 15: 17, 16: 17, 17: 11, 18: 19, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0}
 let dataMap = (key, index) => {
-  let shipments = Object.values(data)[index];
+  let shipments = Object.values(rawData)[index];
   if (shipments == 0) { return };
   let hour = +key;
   let hourLabel = moment(key, 'h').format('hh:mm a');
 
   return { 'hour': hour, 'hourLabel': hourLabel, 'shipments': shipments };
 };
-data = Object.keys(data).map(dataMap).filter(Boolean);
-debugger;
+const mappedData = Object.keys(rawData).map(dataMap).filter(Boolean);
 
-var g = new createjs.Graphics();
-g.setStrokeStyle(0.5);
-g.beginStroke("#000000");
-g.beginFill("green");
-g.moveTo(0,0).lineTo(20,30);
-let newSegment = (x) => g.lineTo(...x);
-values.map( v => newSegment(v));
-stage.addChild(new createjs.Shape(g));
+let addLine = (from, to, stage) => {
+  let g = new createjs.Graphics();
+  g.setStrokeStyle(0.5);
+  g.beginStroke("#000000");
+  g.beginFill("green");
+  g.moveTo(...from).lineTo(...to);
+  stage.addChild(new createjs.Shape(g));
+}
 
-let newCircle = (x) => {
+let addCircle = (x, y, radius, stage) => {
   var circle = new createjs.Shape()
-  circle.graphics.beginFill("Green").drawCircle(...x, 5);
+  circle.graphics.beginFill("Green").drawCircle(x, y, radius);
   stage.addChild(circle)
 }
-values.map( v => newCircle(v));
 
-stage.update();
+let draw = () => {
+  let container = document.getElementById("canvas-container");
+  let width = container.offsetWidth;
+  let height = container.offsetHeight;
+  stage.canvas.width = width;
+  stage.canvas.height = height;
+  addCircle(50,50,10,stage);
+  console.log(width);
+  stage.update();
+}
+window.addEventListener("optimizedResize", draw);
