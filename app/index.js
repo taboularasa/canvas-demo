53627ecfa3e7c70bc967ce1ea75953fa0035e908
David Elliott <david.elliott@stitchfix.com>
2017-05-23 17:48:11 -0700

    can draw lines, and circles from data

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 let moment = require('moment');
 let stage = new createjs.Stage('main-canvas');
+stage.regX = -0.5;
+stage.regY = -0.5;
 
 (function() {
     var throttle = function(type, name, obj) {
@@ -20,6 +22,10 @@ let stage = new createjs.Stage('main-canvas');
     throttle("resize", "optimizedResize");
 })();
 
+function mapRange(in_min, in_max, out_min, out_max, value) {
+  return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 const rawData = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 15, 7: 19, 8: 17, 9: 14, 10: 13, 11: 18, 12: 15, 13: 18, 14: 17, 15: 17, 16: 17, 17: 11, 18: 19, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0}
 let dataMap = (key, index) => {
   let shipments = Object.values(rawData)[index];
@@ -30,30 +36,57 @@ let dataMap = (key, index) => {
   return { 'hour': hour, 'hourLabel': hourLabel, 'shipments': shipments };
 };
 const mappedData = Object.keys(rawData).map(dataMap).filter(Boolean);
+const minShipments = Math.min(...mappedData.map(x => x.shipments));
+const maxShipments = Math.max(...mappedData.map(x => x.shipments))
 
 let addLine = (from, to, stage) => {
   let g = new createjs.Graphics();
-  g.setStrokeStyle(0.5);
-  g.beginStroke("#000000");
+  g.setStrokeStyle(1);
+  g.beginStroke("#666666");
   g.beginFill("green");
   g.moveTo(...from).lineTo(...to);
   stage.addChild(new createjs.Shape(g));
 }
 
-let addCircle = (x, y, radius, stage) => {
+let addCircle = (point, radius, stage) => {
   var circle = new createjs.Shape()
-  circle.graphics.beginFill("Green").drawCircle(x, y, radius);
+  circle.graphics.beginFill("Green").drawCircle(...point, radius);
   stage.addChild(circle)
 }
 
 let draw = () => {
+  stage.removeAllChildren();
   let container = document.getElementById("canvas-container");
   let width = container.offsetWidth;
   let height = container.offsetHeight;
+  let margin = 5;
+  let numWorkHours = 11;
+  let step = width / numWorkHours;
+  var points = [];
   stage.canvas.width = width;
   stage.canvas.height = height;
-  addCircle(50,50,10,stage);
-  console.log(width);
+
+  for (var i = 0; i < numWorkHours; i ++) {
+    let newY = mapRange(minShipments, maxShipments, height - margin, margin, mappedData[i].shipments);
+    let newX = parseInt(i * step + margin);
+    let newPoint = [newX, newY];
+
+    points.push(newPoint);
+  }
+
+  for (let p of points) { addLine([p[0], 0],[p[0], height], stage); }
+
+  var previousPoint;
+  for (let p of points) {
+    if (previousPoint) { addLine(previousPoint, p, stage) }
+    previousPoint = p;
+  }
+
+  for (let p of points) {
+    addCircle(p, 5, stage);
+  }
+
   stage.update();
 }
 window.addEventListener("optimizedResize", draw);
+draw();
